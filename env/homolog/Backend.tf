40d19150438ed817874d793f4cfaa1446887345d
Fabio Siqueira <fabio.siqueira@picpay.com>
2025-06-23 13:41:23 -0300

    remove: hardcoded profile

--- env/homolog/Backend.tf
+++ env/homolog/Backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket = "fast-food-terraform-state"
     key    = "hackathon/terraform.tfstate"
     region = "us-east-1"
-    profile = "fast_food"
   }
 }
--- infra/v1/providers.tf
+++ infra/v1/providers.tf
@@ -2,18 +2,15 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      # version = "6.0.0-beta3"
     }
   }
 }
 
 provider "aws" {
   region  = "us-east-1"
-  profile = "fast_food"
 }
 provider "awscc" {
   region  = "us-east-1"
-  profile = "fast_food"
 }
 
 
