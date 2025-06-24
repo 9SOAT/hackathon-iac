ccdc75fab93bcd33c0611cf745677916e61e8e56
Fabio Siqueira <fabio22.siqueira@gmail.com>
2025-06-24 20:04:13 -0300

    add: simple email service resource (#3)

---
+++ infra/v1/ses.tf
@@ -0,0 +1,9 @@
+resource "aws_ses_email_identity" "semplates_email_identity" {
+  email = var.verified_email
+}
+
+resource "aws_ses_template" "success_email_template" {
+  name = "SUCCESS_EMAIL_TEMPLATE"
+  subject = "Processamento de Vídeo Concluído"
+  html = file("${path.module}/static/success_email_template.html")
+}
