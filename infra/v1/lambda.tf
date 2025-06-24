f5f5733b79d9ad98938f602ed1b87b4adf9b353d
Victor Ambiel <vambiel@fullstacklabs.co>
2025-06-24 20:43:56 -0300

    Provisiona lambda

--- infra/v1/lambda.tf
+++ infra/v1/lambda.tf
@@ -1,13 +1,37 @@
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "${var.projectName}_lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Políticas da Lambda: acesso ao S3 e logs
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Função Lambda (sem código)
 resource "aws_lambda_function" "presigned_url_lambda" {
   function_name = "${var.projectName}_presigned_url"
-  role          = aws_iam_role.lambda_exec_role.arn
+  filename      = null
+  source_code_hash = null
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
-  
-  # Cria função vazia, sem código ainda
-  filename            = null
-  source_code_hash    = null
-  publish             = false
+  role          = aws_iam_role.lambda_exec_role.arn
 
   environment {
     variables = {
