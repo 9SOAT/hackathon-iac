40cd8546d382ba459432b8cdbfaa88717703850e
Fabio Siqueira <fabio.siqueira@picpay.com>
2025-06-27 22:53:16 -0300

    refactor: lambda function resources

---
+++ infra/v1/iam.tf
@@ -0,0 +1,74 @@
+# IAM role for Lambda execution
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+# Lambda Role
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "${var.projectName}_lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+# Policy para a Lambda acessar o S3 e logs
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+
+# SES Email Identity
+data "aws_iam_policy_document" "ses_send_templated_email_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ses:SendTemplatedEmail"
+    ]
+    resources = [
+      aws_ses_email_identity.semplates_email_identity.arn,
+      aws_ses_template.success_email_template.arn
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ses:GetTemplate",
+    ]
+    resources = [aws_ses_template.success_email_template.arn]
+  }
+}
+
+resource "aws_iam_role" "notification_lambda_role" {
+  name = "${var.projectName}_notification_lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "notification_lambda_logs" {
+  role       = aws_iam_role.notification_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_policy" "ses_send_templated_email" {
+  name        = "SESSendTemplatedEmailPolicy"
+  description = "Policy to allow SES sendTemplatedEmail only."
+  policy      = data.aws_iam_policy_document.ses_send_templated_email_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ses_notification_lambda_policy_attach" {
+  role       = aws_iam_role.notification_lambda_role.name
+  policy_arn = aws_iam_policy.ses_send_templated_email.arn
+}
--- infra/v1/lambda.tf
+++ infra/v1/lambda.tf
@@ -1,34 +1,15 @@
-# Lambda Role
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "${var.projectName}_lambda_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action    = "sts:AssumeRole",
-      Effect    = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
-
-# Policy para a Lambda acessar o S3 e logs
-resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+# Package the presigned URL Lambda function code
+data "archive_file" "dummy_presigned_url_zip" {
+  type        = "zip"
+  output_path = "${path.module}/../../lambdas/dummy/presigned_url.zip"
+  source_dir = "${path.module}/../../lambdas/dummy/presigned_url"
 }
 
 # Lambda Function
 resource "aws_lambda_function" "presigned_url_lambda" {
   function_name = "${var.projectName}_presigned_url"
-  filename      = "presigned_dummy.zip"
-  source_code_hash = filebase64sha256("presigned_dummy.zip")
+  filename         = data.archive_file.dummy_presigned_url_zip.output_path
+  source_code_hash = data.archive_file.dummy_presigned_url_zip.output_base64sha256
   handler       = "presigned_dummy.lambda_handler"
   runtime       = "python3.11"
   role          = aws_iam_role.lambda_exec_role.arn
@@ -38,4 +19,22 @@ resource "aws_lambda_function" "presigned_url_lambda" {
       BUCKET_NAME = "presigned-url-fiap-test"
     }
   }
+  tags = var.tags
+}
+
+# Package the notification lambda function code
+data "archive_file" "dummy_email_notification_zip" {
+  type        = "zip"
+  output_path = "${path.module}/../../lambdas/dummy/email_notification.zip"
+  source_dir = "${path.module}/../../lambdas/dummy/email_notification"
+}
+
+resource "aws_lambda_function" "email_notification_lambda" {
+  function_name = "${var.projectName}_email_notification"
+  filename         = data.archive_file.dummy_email_notification_zip.output_path
+  source_code_hash = data.archive_file.dummy_email_notification_zip.output_base64sha256
+  handler       = "main.lambda_handler"
+  runtime       = "python3.11"
+  role          = aws_iam_role.notification_lambda_role.arn
+  tags = var.tags
 }
