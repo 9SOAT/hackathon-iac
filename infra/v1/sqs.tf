1f1bbe16a71c85d6e6b982912cd92d64ed6758f9
francesquini88 <matheus.francesquini@gmail.com>
2025-07-01 19:58:46 -0300

    review:  update files

--- infra/v1/sqs.tf
+++ infra/v1/sqs.tf
@@ -1,28 +1,3 @@
-# resource "aws_sqs_queue" "s3_notifications_sqs" {
-#   name = "s3-notifications-sqs"
-#   policy = <<POLICY
-#   {
-#     "Version": "2012-10-17",
-#     "Statement": [
-#       {
-#         "Effect": "Allow",
-#         "Principal": "*",
-#         "Action": "sqs:SendMessage",
-#         "Resource": "arn:aws:sqs:*:*:s3-notifications-sqs",
-#         "Condition": {
-#           "ArnEquals": {
-#            "aws:SourceArn":
-#               "${aws_s3_bucket.s3_input_bucket.arn}"
-#            }
-#         }
-#       }
-#     ]
-#   }
-#   POLICY
-
-#   tags = var.tags
-# }
-
 resource "aws_sqs_queue" "dlq" {
   name                      = "${var.projectName}-video-processing-queue-dlq"
   message_retention_seconds = 1209600
