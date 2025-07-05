b42460d3ab70f3e19ac5bc494382bf5bb39b81cd
Fabio Siqueira <fabio22.siqueira@gmail.com>
2025-07-05 10:29:45 -0300

    add: pre sign up lambda (#22)

--- infra/v1/lambda.tf
+++ infra/v1/lambda.tf
@@ -2,17 +2,17 @@
 data "archive_file" "dummy_presigned_url_zip" {
   type        = "zip"
   output_path = "${path.module}/../../lambdas/dummy/presigned_url.zip"
-  source_dir = "${path.module}/../../lambdas/dummy/presigned_url"
+  source_dir  = "${path.module}/../../lambdas/dummy/presigned_url"
 }
 
 # Lambda Function
 resource "aws_lambda_function" "presigned_url_lambda" {
-  function_name = "${var.projectName}_presigned_url"
+  function_name    = "${var.projectName}_presigned_url"
   filename         = data.archive_file.dummy_presigned_url_zip.output_path
   source_code_hash = data.archive_file.dummy_presigned_url_zip.output_base64sha256
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.11"
-  role          = aws_iam_role.lambda_exec_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.11"
+  role             = aws_iam_role.lambda_exec_role.arn
 
   environment {
     variables = {
@@ -26,17 +26,17 @@ resource "aws_lambda_function" "presigned_url_lambda" {
 data "archive_file" "dummy_email_notification_zip" {
   type        = "zip"
   output_path = "${path.module}/../../lambdas/dummy/email_notification.zip"
-  source_dir = "${path.module}/../../lambdas/dummy/email_notification"
+  source_dir  = "${path.module}/../../lambdas/dummy/email_notification"
 }
 
 resource "aws_lambda_function" "email_notification_lambda" {
-  function_name = "${var.projectName}_email_notification"
+  function_name    = "${var.projectName}_email_notification"
   filename         = data.archive_file.dummy_email_notification_zip.output_path
   source_code_hash = data.archive_file.dummy_email_notification_zip.output_base64sha256
-  handler       = "main.lambda_handler"
-  runtime       = "python3.11"
-  role          = aws_iam_role.notification_lambda_role.arn
-  tags = var.tags
+  handler          = "main.lambda_handler"
+  runtime          = "python3.11"
+  role             = aws_iam_role.notification_lambda_role.arn
+  tags             = var.tags
 }
 
 # Event source from SQS
@@ -73,19 +73,19 @@ resource "aws_lambda_function" "listing_lambda" {
 data "archive_file" "dummy_processor_zip" {
   type        = "zip"
   output_path = "${path.module}/../../lambdas/dummy/process_video.zip"
-  source_dir = "${path.module}/../../lambdas/dummy/process_video"
+  source_dir  = "${path.module}/../../lambdas/dummy/process_video"
 }
 
 resource "aws_lambda_function" "processor" {
   function_name    = "${var.projectName}_video_processor"
   filename         = data.archive_file.dummy_processor_zip.output_path
   source_code_hash = data.archive_file.dummy_processor_zip.output_base64sha256
-  handler       = "lambda_processor.lambda_handler"
-  runtime       = "python3.11"
+  handler          = "lambda_processor.lambda_handler"
+  runtime          = "python3.11"
   timeout          = 900
   memory_size      = 1024
-  layers           = ["arn:aws:lambda:us-east-1:897722698720:layer:ffmpeg:1"]
-  role          = aws_iam_role.lambda_exec.arn
+  layers = ["arn:aws:lambda:us-east-1:897722698720:layer:ffmpeg:1"]
+  role             = aws_iam_role.lambda_exec.arn
 
   environment {
     variables = {
@@ -101,4 +101,31 @@ resource "aws_lambda_event_source_mapping" "sqs2lambda" {
   function_name    = aws_lambda_function.processor.arn
   batch_size       = 1
   enabled          = true
+}
+
+# Package the pre sign up Lambda function code
+data "archive_file" "pre_sign_up" {
+  type        = "zip"
+  output_path = "${path.module}/../../lambdas/dummy/pre_sign_up.zip"
+  source_dir  = "${path.module}/../../lambdas/dummy/pre_sign_up"
+}
+
+# Lambda Function
+resource "aws_lambda_function" "pre_sign_up" {
+  function_name    = "${var.projectName}_pre_sign_up"
+  filename         = data.archive_file.pre_sign_up.output_path
+  source_code_hash = data.archive_file.pre_sign_up.output_base64sha256
+  handler          = "main.lambda_handler"
+  runtime          = "python3.11"
+  role             = aws_iam_role.lambda_exec_role.arn
+
+  tags = var.tags
+}
+
+resource "aws_lambda_permission" "cognito_trigger" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.pre_sign_up.function_name
+  principal     = "cognito-idp.amazonaws.com"
+  source_arn    = aws_cognito_user_pool.pool.arn
+  statement_id  = "AllowCognitoInvocation"
 }
